060f16083509212b352aba303b24ee561adc4b6a
Florian <f.kuhlenbeck@gmail.com>
2021-07-21 15:24:43 +0200

    initial commit

---
+++ Fernbedienung/Fernbedienung.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fernseher
+{
+    class Fernbedienung
+    {
+        public bool isOn = false;
+
+        public void operation()
+        {
+            do
+
+            {
+                string userInput = "";
+                do
+                {
+                    Console.WriteLine("Drücken Sie folgende Tasten:");
+                    Console.WriteLine("[n] für den nächsten Kanal");
+                    Console.WriteLine("[p] für den vorherigen Kanal");
+                    Console.WriteLine("[+] um den Fernseher lauter zu schalten");
+                    Console.WriteLine("[-] um den Fernseher leiser zu schalten");
+                    Console.WriteLine("[o] um den Fernseher auszuschalten");
+                    userInput = Console.ReadLine();
+
+                }
+                while (!(userInput == "x" || userInput == "n" || userInput == "p" || userInput == "+" || userInput == "-" || userInput == "o"));
+
+                if (userInput == "x")
+                {
+                    isOn = true;
+                }
+                else if (userInput == "o")
+                {
+                    isOn = false;
+                }
+                else if (userInput == "+")
+                {
+                    //volumneUp();
+                }
+                else if (userInput == "-")
+                {
+                    //volumneDown();
+                }
+                else if (userInput == "n")
+                {
+                    //nextChannel();
+                }
+                else
+                {
+                    //previousChannel();
+                }
+            }
+            while (isOn = true);
+        }
+    }
+}
---
+++ Fernbedienung/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Fernseher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fernbedienung fernbedienung = new Fernbedienung();
+
+            var channelList = Enumerable.Range(1, 10).ToList();
+
+            do
+            {
+                Console.WriteLine($"Der Fernseher ist aus, drücken Sie [x] um den Fernseher einzuschalten");
+            }
+            while (fernbedienung.isOn = false);
+            if(fernbedienung.isOn = true)
+            {
+                fernbedienung.operation();
+
+            }
+
+
+        }
+    }
+}
